55b92865eb1e50daa5750e4b0570eeceb2034c1f
OursPhilosophe <102171881+OursPhilosophe@users.noreply.github.com>
2022-05-14 13:03:21 +0200

    Add files via upload

--- dico.c
+++ dico.c
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int length=835;
+#include "dico.h"
 
 
-void lecture(char* filename,char** L)
+
+void lecture(char* filename,char** L,int s)
 {
 
 	FILE* f=fopen(filename,"rb");
@@ -18,29 +19,31 @@ void lecture(char* filename,char** L)
 		printf("Impossible d'ouvrir le fichier");
 	}
 	L[0]=malloc(sizeof(char*));
-    fscanf(f,"%s",L[0]); 
-    for (int i=1;i<length;i++)
+	char buffer[50];
+    fscanf(f,"%s",buffer);
+	strcpy(L[0],buffer);
+    for (int i=1;i<s;i++)
 	{
         L[i]=malloc(sizeof(char*));
-		fscanf(f,"%s",L[i]);
+		fscanf(f,"%s",buffer);
+		strcpy(L[i],buffer);
 	}
-	L[length]="\0";
 	
     fclose(f);
 
 }
 
-char* mot_aleatoire(int l,char* filename,char** L)
+char* mot_aleatoire(int l,char* filename,char** L,int s)
 {
-	char* liste_mots[length];
+	char* liste_mots[s];
 	if (L==NULL)
 	{
 		printf("L'allocation a échoué");
 		return NULL;
 	}
-	lecture(filename,L);
+	lecture(filename,L,s);
 	int j=0;
-	for (int i=0; i<length; i++) 
+	for (int i=0; i<s; i++) 
 	{		
 		if (strlen(L[i])==l) 
 		{
@@ -55,18 +58,18 @@ char* mot_aleatoire(int l,char* filename,char** L)
 
 }
 
-
 /*
+
 int main ()
 {
 	int longueur;
-	char** L=malloc(835*sizeof(char*));
+	char** L=malloc(386264*sizeof(char*));
 	printf("Rentrez une longueur de mot :\n");
 	scanf("%d",&longueur);
-	char* mot = mot_aleatoire(longueur,"liste_francais.txt",L);
+	char* mot = mot_aleatoire(longueur,"meilleur.txt",L,386264);
 	printf("le mot est : %s\n", mot);
 	return 0;	
 }
-*/
 
+*/
 
--- dico.h
+++ dico.h
@@ -1,11 +1,10 @@
 #ifndef __DICO_H__
 #define __DICO_H__
 
-void lecture(char* filename,char** L);
+void lecture(char* filename,char** L,int s);
 
-char* mot_aleatoire(int l,char* filename,char** L);
+char* mot_aleatoire(int l,char* filename,char** L,int s);
 
-extern int length;
 
 #endif
 
